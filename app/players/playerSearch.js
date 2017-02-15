77eb86813af1155c0f2c5dce793656a484e32902
Dheeraj D Bhat <dheerajbhat@infrrd.ai>
2017-02-15 18:10:46 +0530

    search included

--- app/players/playerSearch.js
+++ app/players/playerSearch.js
@@ -12,5 +12,24 @@ angular.module('InfrrdPL.players',['ui.router'])
 
 .controller('playerCtrl',['$scope','$state',function($scope,$state){
 
+  var playerArray = [
+    {no: 1, name: "Dheeraj Bhat", role: "Batsman", team: "RedDevils"},
+    {no: 2, name: "RedDevil", role: "Captain/All Rounder", team: "RedDevils"},
+    {no: 3, name: "Mastermind", role: "Bowler", team: "RedDevils"}
+  ];
+
+  $scope.playersSearched = playerArray;
+
+  $scope.searchThePlayer = function(){
+    var player = $scope.player;
+
+    if(player){
+      $scope.playerFound = true;
+      $scope.errMsg = false;
+    }else{
+      $scope.errMsg = true;
+      $scope.playerFound = false;
+    }
+  }
 
 }])
